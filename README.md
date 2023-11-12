c5fdf19a1a06a42fab6a80aa5bac2e7710318c14
mkfreeman <michael.k.freeman@gmail.com>
2023-11-11 21:13:55 -0500

    Add points at stops

--- README.md
+++ README.md
@@ -5,7 +5,9 @@
   repo](https://github.com/ShipBit/sveltekit-leaflet/), referenced in this
   (actually informative) [video](https://www.youtube.com/watch?v=JFctWXEzFZw&ab_channel=ShipBit)
 - Turns out configuring TailwindCSS with Svelte is a bit of a pain, used
-  [svelte-add](https://github.com/svelte-add/svelte-add) to do this: `npx --yes svelte-add@latest tailwindcss`
+  [svelte-add](https://github.com/svelte-add/svelte-add) to do this: `npx --yes
+  svelte-add@latest tailwindcss`
+- Stops downloaded from [here](https://ckan0.cf.opendata.inter.prod-toronto.ca/dataset/7795b45e-e65a-4465-81fc-c36b9dfff169/resource/cfb6b2b8-6191-41e3-bda1-b175c51148cb/download/opendata_ttc_schedules.zip)
 
 
 _information below is from the original development set up template_
--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,30 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+  import { onMount } from "svelte"
   import Leaflet from './lib/Leaflet.svelte'
+  import Marker from './lib/Marker.svelte'
   import type { LatLngExpression } from 'leaflet';
-  const initialView: LatLngExpression = [43.651070, -79.347015]; // Dortmund, Germany
+  const initialView: LatLngExpression = [43.651070, -79.397015];
+  import * as d3 from "d3";
+	let data;
+  onMount(
+		async () => {
+      // TODO: filter down to pre-set bounds
+			data = await d3.csv('./stops.txt')
+		}
+	)
 </script>
 
 <div class="w-full h-screen">
-  <Leaflet view={initialView} zoom={13}/>
+  <Leaflet view={initialView} zoom={14}>
+    {#if data}
+      {#each data as stop}
+        <Marker latLng={[+stop.stop_lat, +stop.stop_lon]} width={20} height={20}>
+          <div class="bg-black w-3 h-3 rounded-full"></div>
+        </Marker>
+      {/each}
+    {/if}
+    </Leaflet>
+
 </div>
   
 
