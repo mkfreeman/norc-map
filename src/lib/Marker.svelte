4b3b7f174e28e45bc0ab4c0182acaf60d40fb2ea
mkfreeman <michael.k.freeman@gmail.com>
2023-11-17 12:33:21 -0500

    Add additional map for exploring stops, popups don't work after update

--- src/lib/Marker.svelte
+++ src/lib/Marker.svelte
@@ -1,29 +1,51 @@
 <script lang="ts">
-  import { onMount, onDestroy, getContext, setContext } from "svelte";
+  import {
+    onMount,
+    onDestroy,
+    getContext,
+    setContext,
+    beforeUpdate,
+    afterUpdate,
+  } from "svelte";
   import L from "leaflet";
-  
-  export let latLng: L.LatLngExpression;  
-  export let iconUrl = "./ttc.svg";
+
+  export let latLng: L.LatLngExpression;
+  export let iconUrl;
   export let iconSize = [50, 50];
+  export let radius = 4;
+  export let fillColor = "blue";
 
   let marker: L.CircleMarker | undefined;
   let markerElement: HTMLElement;
 
   const { getMap }: { getMap: () => L.Map | undefined } = getContext("map");
   const map = getMap();
-  
+
   setContext("layer", {
     // L.Marker inherits from L.Layer
     getLayer: () => marker,
   });
 
+  function makeMarker() {
+    marker = iconUrl
+      ? L.marker(latLng, {
+          icon: L.icon({
+            iconUrl,
+            iconSize,
+          }),
+        }).addTo(map)
+      : L.circleMarker(latLng, { radius, fillColor, stroke: false }).addTo(map);
+  }
+
   onMount(() => {
-    if (map) {		
-      let icon = L.icon({
-        iconUrl,
-        iconSize,
-      });
-      marker = L.marker(latLng, { icon }).addTo(map);     
+    if (map) makeMarker();
+  });
+
+  // TODO: this is redundant....
+  afterUpdate(() => {
+    if (marker && map) {
+      marker.remove();
+      makeMarker();
     }
   });
 
---
+++ src/lib/Radio.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+  interface RadioOption {
+    value: string;
+    label: string;
+  }
+
+  export let options: RadioOption[] = [];
+  export let selected: string = '';
+  export let updateMethod: (selected: string) => void = () => {};
+  export let label: string = '';
+
+  function handleRadioChange(event: Event) {
+    const target = event.target as HTMLInputElement;
+    selected = target.value;
+    updateMethod(selected);
+  }
+</script>
+
+<div class="flex items-center">
+  {#if label}
+    <strong class="mr-2">{label}</strong>
+  {/if}
+
+  {#each options as option}
+    <label class="inline-flex items-center mr-4">
+      <input
+        type="radio"
+        class="form-radio text-indigo-600"
+        bind:group={selected}
+        value={option.value}
+        on:change={handleRadioChange}
+      />
+      <span class="ml-2">{option.label}</span>
+    </label>
+  {/each}
+</div>
